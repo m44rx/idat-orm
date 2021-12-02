8e9e72694a7fd4f44d2f8b70e7024051a7b4bf2a
Usuario1 <Usuario1@Wilder.home>
2021-12-02 17:56:56 -0500

    JWT

--- src/main/java/com/example/serviciomatricula/security/ConfigSecurity.java
+++ src/main/java/com/example/serviciomatricula/security/ConfigSecurity.java
@@ -3,19 +3,35 @@ package com.example.serviciomatricula.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	private MyUserDetailService userDetailService;
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+//	@Autowired
+//	private MyUserDetailService userDetailService;
+	
+	@Autowired
+	private UserDetailsService userDetailService;
+	
+	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,17 +43,36 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/curso/listar").hasAnyRole("INSTRUCTOR","USER");
-//			.antMatchers("/curso/listar").permitAll()
-//			.antMatchers("/curso/listar").access("hasRole('INSTRUCTOR')");
-		http.authorizeRequests().and().httpBasic();
-		http.authorizeRequests().and().csrf().disable();
+//		http.authorizeRequests()
+//			.antMatchers("/curso/listar").hasAnyRole("INSTRUCTOR","USER")
+//			.antMatchers("/curso/listar").permitAll()		
+//			.antMatchers("/curso/listar").access("hasRole('INSTRUCTOR')");			
+//		http.authorizeRequests().and().httpBasic();
+//		http.authorizeRequests().and().csrf().disable();
+		
+		// We don't need CSRF for this example
+		http.csrf().disable()
+			    // dont authenticate this particular request
+			    .authorizeRequests().antMatchers("/authenticate").permitAll().
+			    // all other requests need to be authenticated
+			    anyRequest().authenticated().and().
+			    // make sure we use stateless session; session won't be used to store user's state.
+					exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+	                           .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// Add a filter to validate the tokens with every request
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
 
 	}
 
 	@Bean
 	public PasswordEncoder password() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
---
+++ target/classes/import.sql
@@ -0,0 +1,10 @@
+insert into profesores( nombre, apellido, usuario, contrasenia, rol) values ('Wilder','Espinoza', 'wespinoza','123','INSTRUCTOR')
+insert into profesores( nombre, apellido, usuario, contrasenia, rol) values ('Juan','Valdez', 'jvaldez', '123','INSTRUCTOR')
+insert into profesores( nombre, apellido, usuario, contrasenia, rol) values ('Karina','Ramos', 'kramos', '123','USER')
+
+	
+insert into matriculas( anio, ciclo_academico) values (2021,'2021-I')
+insert into matriculas( anio, ciclo_academico) values (2022,'2022-I')
+
+insert into cursos( nombre, descripcion) values ('Microservicios I','Curso sobre javax y rsjava')
+insert into cursos( nombre, descripcion) values ('Microservicios II','Curso sobre reactividad, microservicios, webflux')
